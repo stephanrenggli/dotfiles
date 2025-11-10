a7f486a521bf713375bced5681ef3aefbab3e47d
Stephan Renggli <stephan.renggli@hslu.ch>
2025-11-10 16:52:38 +0100

    feat: add loop

--- .chezmoiscripts/run_onchange_01-install-brew.sh
+++ .chezmoiscripts/run_onchange_01-install-brew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if  command -v brew >/dev/null 2>&1; then 
+if  command -v brew >/dev/null 2>&1; then
     echo "brew already installed"
 else
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
